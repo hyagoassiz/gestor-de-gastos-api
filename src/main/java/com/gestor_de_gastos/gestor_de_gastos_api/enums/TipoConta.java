8b0174e7994850ca68dce5257aeb5591c6b9ef3c
hyagoassiz <hyagoassiz@hotmail.com>
2025-12-06 14:11:05 -0300

    ajuste na dashboard

--- src/main/java/com/gestor_de_gastos/gestor_de_gastos_api/enums/TipoConta.java
+++ src/main/java/com/gestor_de_gastos/gestor_de_gastos_api/enums/TipoConta.java
@@ -3,8 +3,10 @@ package com.gestor_de_gastos.gestor_de_gastos_api.enums;
 public enum TipoConta {
 
     CONTA_CORRENTE("Conta Corrente"),
+    DINHEIRO("Dinheiro"),
     INVESTIMENTO("Investimento"),
     POUPANCA("Poupança"),
+    VR_VA("VR/VA"),
     OUTROS("Outros");
 
     private final String descricao;
